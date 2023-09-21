204be7695b7d147e0203b41d97abb91e1a955300
marcosmas28 <marcos.schonfeld@gmail.com>
2023-09-21 18:23:16 -0300

    Exception Handling

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Ansible Collection - octupus.o4n_ros
+# Ansible Collection - octupus.o4n_ruggedcom_ros
 
 Collection to automate tasks on Ruggedcom ROS devices.
 
 ## Modules
 
-- o4n_ros_command: Executes arbitrary commands against a Ruggedcom ROS device.
+- o4n_ros_command: Executes arbitrary commands against a Ruggedcom ROS device. it works with Telnet and SSH protocols.
 - o4n_ros_facts (planned): Gather facts on Ruggedcom ROS devices and return as ansible_facts.
 
 ## Requirements
--- plugins/modules/o4n_ros_command.py
+++ plugins/modules/o4n_ros_command.py
@@ -8,7 +8,7 @@ module: o4n_ros_command
 
 short_description: Ansible module for executing commands in Ruggedcom ROS devices
 
-version_added: "1.0.2"
+version_added: "1.0.3"
 
 description: Ansible module for executing commands in Ruggedcom ROS devices.
 
@@ -40,6 +40,11 @@ options:
         description: List of commands to execute on the Ruggedcom OS device.
         required: true
         type: list
+    telnet_timeout:
+        description: Number of seconds to wait for answers from the device.
+        required: false
+        type: int
+        default: 10
 
 requirements:
     - netmiko
@@ -85,6 +90,7 @@ from ansible.module_utils.basic import AnsibleModule
 from netmiko import ConnectHandler, NetmikoTimeoutException
 import telnetlib
 import time
+import logging
 
 
 def main():
@@ -96,6 +102,7 @@ def main():
             user=dict(required=True, type='str'),
             password=dict(required=True, type='str', no_log=True),
             commands=dict(required=True, type='list'),
+            telnet_timeout=dict(required=False, type='int', default=10),
         )
     )
 
@@ -105,23 +112,62 @@ def main():
     user = module.params.get("user")
     password = module.params.get("password")
     commands = module.params.get("commands")
+    telnet_timeout = module.params.get("telnet_timeout")
 
     module_success = False
 
+    # Prevent Paramiko logs
+    logger = logging.getLogger("paramiko")
+    logger.addHandler(logging.NullHandler())
+
     if protocol == "telnet":
-        tn_client = telnetlib.Telnet(host=host, port=port)
+        try:
+            tn_client = telnetlib.Telnet(host=host, port=port, timeout=telnet_timeout)
+        except Exception as err:
+            ret_msg = 'O4N_ERROR: Telnet Connection Exception (a)\nConnection settings: ' + user + '@' +  host + ':' + str(port) + '\n' + str(err)
+            module.fail_json(msg=ret_msg, failed=True, changed=False)
+            return()
         # send user
-        tn_client.read_until(b'Enter User Name: ')
+        try:
+            tn_client.read_until(b'Enter User Name: ', telnet_timeout)
+        except Exception as err:
+            ret_msg = 'O4N_ERROR: Telnet Connection Exception (b)\nConnection settings: ' + user + '@' +  host + ':' + str(port) + '\n' + str(err)
+            module.fail_json(msg=ret_msg, failed=True, changed=False)
+            return()
         tn_client.write(user.encode() + b'\n')
         # send password
-        tn_client.read_until(b'Password: ')
+        try:
+            tn_client.read_until(b'Password: ', telnet_timeout)
+        except Exception as err:
+            ret_msg = 'O4N_ERROR: Telnet Connection Exception (c)\nConnection settings: ' + user + '@' +  host + ':' + str(port) + '\n' + str(err)
+            module.fail_json(msg=ret_msg, failed=True, changed=False)
+            return()
         tn_client.write(password.encode() + b'\n')
         # Enter CLI prompt
         tn_client.write(b'\x13')
         tn_client.write(b'cls' + b'\n')
         time.sleep(1)
-        tn_client.read_very_eager()
+        try:
+            tn_client.read_very_eager()
+        except Exception as err:
+            ret_msg = 'O4N_ERROR: Telnet Connection Exception (d)\nConnection settings: ' + user + '@' +  host + ':' + str(port) + '\n' + str(err)
+            module.fail_json(msg=ret_msg, failed=True, changed=False)
+            return()
         tn_client.write(b'\n')
+
+        # Check Authentication
+        prompt = tn_client.read_until(b'>    ', telnet_timeout)
+        if 'Enter User Name:' in prompt.decode('ascii'):
+            ret_msg = """
+'O4N_ERROR: Telnet Authentication Exception.\n
+Authentication to device failed.\n
+Common causes of this problem are:\n
+1. Invalid username and password\n
+2. Connecting to the wrong device\n
+Connection settings:
+"""
+            module.fail_json(msg=ret_msg + user + '@' +  host + ':' + str(port) , failed=True, changed=False)
+            return()
 
         for command in commands:
             time.sleep(0.2)
@@ -129,7 +175,12 @@ def main():
 
         # send finish keyword and read output
         tn_client.write(b'    '+b'\n')
-        output = tn_client.read_until(b'>    ')
+        try:
+            output = tn_client.read_until(b'>    ')
+        except Exception as err:
+            ret_msg = 'O4N_ERROR: Telnet Command Exception \n' + str(err)
+            module.fail_json(msg=ret_msg, failed=True, changed=False)
+            return()
         output = output.decode('ascii')
         # close session
         tn_client.close()
@@ -139,23 +190,43 @@ def main():
         if module_success:
             module.exit_json(failed=False, content=output)
         else:
-            module.exit_json(failed=True, content=output)
+            module.fail_json(msg='O4N_ERROR: Module Failed\n', failed=True, changed=False)
 
     else:
-        ssh_client = ConnectHandler(host, device_type='autodetect', username=user, port=port, password=password, auth_timeout=90, timeout=60)
-        ssh_client.send_command_timing('\n \x13', last_read=1)
+        try:
+            ssh_client = ConnectHandler(host, device_type='autodetect', username=user, port=port, password=password, auth_timeout=90, timeout=60)
+        except Exception as err:
+            ret_msg = 'O4N_ERROR: SSH Connection Exception\n' + str(err)
+            module.fail_json(msg=ret_msg, failed=True, changed=False)
+            return()
+        try:
+            ssh_client.send_command_timing('\n \x13', last_read=1)
+        except Exception as err:
+            ret_msg = 'O4N_ERROR: CLI Prompt Exception\n' + str(err)
+            module.fail_json(msg=ret_msg, failed=True, changed=False)
+            return()
         output = ""
         for command in commands:
             # Aux Vars:
             more_prompt = "--More-- or (q)uit"
             cmd_output = '>' + command + '\n'
             # Execute command:
-            page = ssh_client.send_command_timing(command, last_read=1)
+            try:
+                page = ssh_client.send_command_timing(command, last_read=1)
+            except Exception as err:
+                ret_msg = 'O4N_ERROR: Send Command Exception\n' + str(err)
+                module.fail_json(msg=ret_msg, failed=True, changed=False)
+                return()
             while True:
                 try:
                     cmd_output += page
                     if more_prompt in page:
-                        page = ssh_client.send_command_timing('\n', last_read=1)
+                        try:
+                            page = ssh_client.send_command_timing('\n', last_read=1)
+                        except Exception as err:
+                            ret_msg = 'O4N_ERROR: Pagination Exception\n' + str(err)
+                            module.fail_json(msg=ret_msg, failed=True, changed=False)
+                            return()
                     else:
                         break
                 except NetmikoTimeoutException:
@@ -170,8 +241,9 @@ def main():
         if module_success:
             module.exit_json(failed=False, content=output)
         else:
-            module.exit_json(failed=True, content=output)
+            module.fail_json(msg='O4N_ERROR: Module Failed\n', failed=True, changed=False)
 
 
 if __name__ == "__main__":
     main()
+
